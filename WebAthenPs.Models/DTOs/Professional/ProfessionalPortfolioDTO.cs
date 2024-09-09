a4d13eeef3d4f7b61633f37730cb7e7190cfe3e3
BrunoHGG9 <hgc9bruno@gmail.com>
2024-09-09 05:31:52 -0300

    #4B ProfessionalPotfolioDTO

---
+++ WebAthenPs.Models/DTOs/Professional/ProfessionalPortfolioDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAthenPs.Models.DTOs.Professional
+{
+    public class ProfessionalPortfolioDTO
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string Address { get; set; }
+        public DateOnly CreatedAt { get; set; }
+        public List<string> ProfessionalTypes { get; set; } = new List<string>();
+        public List<ProjectProfessionalDTO> ProjectProfessionals { get; set; } = new List<ProjectProfessionalDTO>();
+        
+    }
+}
