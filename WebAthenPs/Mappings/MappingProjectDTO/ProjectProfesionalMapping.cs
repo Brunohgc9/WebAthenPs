b34d5a9cf4cfb9e92cef3fdd1f186192196efd66
BrunoHGG9 <hgc9bruno@gmail.com>
2024-10-15 06:14:25 -0300

    ProjectProfessionalMapping

---
+++ WebAthenPs/Mappings/MappingProjectDTO/ProjectProfesionalMapping.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WebAthenPs.API.Entities.Project;
+using WebAthenPs.Models.DTOs.Professional;
+
+namespace WebAthenPs.API.Mappings.MappingProjectDTO
+{
+    public static class ProjectProfessionalMapping
+    {
+        // Converte uma lista de ProjectProfessional para DTO
+        public static IEnumerable<ProjectProfessionalDTO> ConverterProjectProfessionalsParaDTO(
+            this IEnumerable<ProjectProfessional> projectProfessionals)
+        {
+            return (from projectProfessional in projectProfessionals
+                    select new ProjectProfessionalDTO
+                    {
+                        ProfessionalId = projectProfessional.ProfessionalId,
+                        ProjectId = projectProfessional.ProjectId,
+                        Professional = projectProfessional.Professional != null
+                            ? new GenericProfessionalDTO
+                            {
+                                Id = projectProfessional.Professional.Id,
+                                UserId = projectProfessional.Professional.UserId,
+                                UserName = projectProfessional.Professional.User?.UserName,
+                                Email = projectProfessional.Professional.User?.Email,
+                                PhoneNumber = projectProfessional.Professional.User?.PhoneNumber
+                            }
+                            : null,
+                        ContractedAs = projectProfessional.ContractedAs.ToList()
+                    }).ToList();
+        }
+
+        // Converte um ProjectProfessional individual para DTO
+        public static ProjectProfessionalDTO ConverterProjectProfessionalParaDTO(this ProjectProfessional projectProfessional)
+        {
+            return new ProjectProfessionalDTO
+            {
+                ProfessionalId = projectProfessional.ProfessionalId,
+                ProjectId = projectProfessional.ProjectId,
+                Professional = projectProfessional.Professional != null
+                    ? new GenericProfessionalDTO
+                    {
+                        Id = projectProfessional.Professional.Id,
+                        UserId = projectProfessional.Professional.UserId,
+                        UserName = projectProfessional.Professional.User?.UserName,
+                        Email = projectProfessional.Professional.User?.Email,
+                        PhoneNumber = projectProfessional.Professional.User?.PhoneNumber
+                    }
+                    : null,
+                ContractedAs = projectProfessional.ContractedAs.ToList()
+            };
+        }
+
+        // Converte um DTO para entidade de ProjectProfessional
+        public static ProjectProfessional ConverterDTOParaProjectProfessional(this ProjectProfessionalDTO dto)
+        {
+            return new ProjectProfessional
+            {
+                ProfessionalId = dto.ProfessionalId,
+                ProjectId = dto.ProjectId,
+                ContractedAs = dto.ContractedAs
+            };
+        }
+    }
+}
